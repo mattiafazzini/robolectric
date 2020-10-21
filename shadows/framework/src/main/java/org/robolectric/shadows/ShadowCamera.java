e796a6b477c7688599917d0b2447735df09cdf42
Googler <noreply@google.com>
2020-10-21 10:00:33 -0700

    Update ShadowCamera to support enabling/disabling the shutter sound during image capture. This should avoid NPE in tests using this API.
    PiperOrigin-RevId: 338284364

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowCamera.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowCamera.java
@@ -1,5 +1,6 @@
 package org.robolectric.shadows;
 
+import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR1;
 import static org.robolectric.shadow.api.Shadow.newInstanceOf;
 
 import android.graphics.ImageFormat;
@@ -33,6 +34,7 @@ public class ShadowCamera {
   private int displayOrientation;
   private Camera.AutoFocusCallback autoFocusCallback;
   private boolean autoFocusing;
+  private boolean shutterEnabled;
 
   private static Map<Integer, Camera.CameraInfo> cameras = new HashMap<>();
 
@@ -43,6 +45,7 @@ public class ShadowCamera {
     locked = true;
     previewing = false;
     released = false;
+    shutterEnabled = true;
   }
 
   @Implementation
@@ -211,6 +214,19 @@ public class ShadowCamera {
     if (jpeg != null) {
       jpeg.onPictureTaken(new byte[0], realCamera);
     }
+  }
+
+  @Implementation(minSdk = JELLY_BEAN_MR1)
+  protected boolean enableShutterSound(boolean enabled) {
+    if (!enabled && cameras.containsKey(id) && !cameras.get(id).canDisableShutterSound) {
+      return false;
+    }
+    shutterEnabled = enabled;
+    return true;
+  }
+
+  public boolean isShutterEnabled() {
+    return shutterEnabled;
   }
 
   public boolean isLocked() {
